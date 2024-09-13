d804ad5373b9cd174e6132767f9c1e8d88fa8180
sekhar <deals4sekhar@gmail.com>
2024-09-13 14:10:04 -0400

    website changed

--- src/test/java/stepDefinitions/Login.java
+++ src/test/java/stepDefinitions/Login.java
@@ -22,46 +22,70 @@ public class Login {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		//driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
-		driver.get("https://www.amazon.com");
+		driver.get("https://practicetestautomation.com/practice-test-login/");
 		System.out.println("application launched sucessfully");
 	}
 
+	/*
 	@When("the user enters valid credentials \\(username: {string}, password: {string})")
 	public void enter_UserID_And_Pass(String user, String pwd) {
 		
-		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys(user);
+		//driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys(user);
 		
-		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys(pwd);
+		//driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys(pwd);
 		
-		System.out.println("entered valid userID and password");
+		//System.out.println("entered valid userID and password");
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(user);
+		
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(pwd);
 	    
 	}
+	*/
+	
+	@When("the user enters valid credentials \\(username:{string}, password: {string})")
+	public void the_user_enters_valid_credentials_username_password(String user, String pwd) {
+	    
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(user);
+		
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(pwd);
+	    
+	}
+	
 
 	@When("the user clicks on the Login button")
 	public void click_on_login_button() {
 		
-		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		//driver.findElement(By.xpath("//input[@value='Login']")).click();
 		
-		System.out.println("clicked on login button");
+		//System.out.println("clicked on login button");
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+		
 	}
 
 	@Then("the user should be redirected to the My Account page")
 	public void validate_my_account_page() {
+		 
+		boolean status1=driver.findElement(By
+				.xpath("//h1[normalize-space()='Logged In Successfully']")).isDisplayed();
 		
-		 boolean status=driver.findElement(By.xpath("//*[@id=\"content\"]/h2[1]")).isDisplayed();
+		boolean status2=driver.findElement(By
+				.xpath("//strong[contains(text(),'Congratulations student. You successfully logged i')]")).isDisplayed();
+		
+		boolean status3=driver.findElement(By
+				.xpath("//a[normalize-space()='Log out']")).isDisplayed();
+		 //System.out.println("My Account text is displayed");
 		 
-		 System.out.println("My Account text is displayed");
+		 Assert.assertEquals(status1,true); 
+		 Assert.assertEquals(status2,true);
+		 Assert.assertEquals(status3,true);
 		 
-		 Assert.assertEquals(status,true);   
 	}
 
 	@Then("the user should see a welcome message")
 	public void the_user_should_see_a_welcome_message() {
 		
-		driver.findElement(By.xpath("//*[@id='column-right']/div/a[13]")).click();
-		
-		System.out.println("clicked on logout button");
-		
+		driver.findElement(By.xpath("//a[normalize-space()='Log out']")).click();
+				
 		driver.quit();
 	}
 
--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,8 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {".//Feature/login.feature"}, glue="stepDefinitions")
-
+@CucumberOptions(features= {".//Feature/login.feature"}, glue={"stepDefinitions"},
+dryRun=false
+)
 
 public class TestRunner {
 
